b1da7ea7b6d419d0b93d5e0d475ab4ff07743e4e
Jakub Sikora <jakub.sikora@pure360.com>
2016-04-12 08:43:08 +0100

    testing envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -4,7 +4,7 @@
     {{-- Target the project directory --}}
     cd /var/www/football-api
     {{-- Set app to maintenance mode --}}
-    php artisan down
+    <!-- php artisan down -->
     {{-- Update composer to latest version --}}
     composer self-update
     {{-- Pull latest changes from Repository --}}
@@ -12,7 +12,7 @@
     {{-- Install project dependencies without development dependencies and without interaction --}}
     composer install --prefer-source --no-interaction --no-dev
     {{-- If there is anything to migrate, migrate it --}}
-    php artisan migrate
+    <!-- php artisan migrate -->
     {{-- Disable maintenance mode --}}
-    php artisan up
+    <!-- php artisan up -->
 @endtask
