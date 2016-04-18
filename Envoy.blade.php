c2c932ae72e98c96148f4ed0b482f9a0eff90a21
Jakub Sikora <jakub.sikora.en@gmail.com>
2016-04-18 13:15:06 +0100

    Update Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,35 +1,6 @@
-<?php
-$server = 'jakub@jsikora.io';
-$project_dir = '/var/www/football-api';
-?>
+@servers(['web' => 'jakub@jsikora.io' ])
 
-@servers(['web' => {{ $server }}])
-
-@macro('deploy', ['on' => 'web'])
-    fetch_repo
-    enable_maintenance_mode
-    run_composer
-    migration
-    disable_maintenance_mode
-@endmacro
-
-@task('fetch_repo')
-    cd {{ $project_dir }};
+@task('deploy', ['on' => 'web'])
+    cd /var/www/football-api;
     git pull origin master;
-@endtask
-
-@task('run_composer')
-    composer install --prefer-dist;
-@endtask
-
-@task('enable_maintenance_mode')
-    {{-- php artisan down -}}
-@endtask
-
-@task('migration')
-    {{-- php artisan migrate -}}
-@endtask
-
-@task('disable_maintenance_mode')
-    {{-- php artisan up -}}
 @endtask
