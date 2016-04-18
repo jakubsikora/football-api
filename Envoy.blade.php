303cb7a5914b3c932a1e357516f595be9f3b37b5
Jakub Sikora <jakub.sikora.en@gmail.com>
2016-04-18 13:22:24 +0100

    Update Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,4 +3,7 @@
 @task('deploy', ['on' => 'web'])
     cd /var/www/football-api;
     git pull origin master;
+    php artisan down;
+    composer install --prefer-dist;
+    php artisan up;
 @endtask
